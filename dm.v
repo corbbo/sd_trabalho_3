ac5f45e5b7803aa1c723202228b6c6c42431ec0a
Leo Rosa <126796587+pastorfred@users.noreply.github.com>
2023-11-09 18:18:55 -0300

    Update dm.v

--- dm.v
+++ dm.v
@@ -14,14 +14,14 @@ assign d4 = (data_2 / 1000) % 10;
 dspl_drv_NexysA7 driver (
   .reset(rst), 
   .clock(clk), 
-  .d1({1, d1, 0}), 
-  .d2({1, d2, 0}), 
-  .d3({1, d3, 0}), 
-  .d4({1, d4, 0}), 
+  .d1({1'b1, d1, 1'b0}), 
+  .d2({1'b1, d2, 1'b0}), 
+  .d3({1'b1, d3, 1'b0}), 
+  .d4({1'b1, d4, 1'b0}), 
   .d5(0), 
-  .d6({1, modulo, 0}), 
+  .d6({1'b1, modulo, 1'b0}), 
   .d7(0), 
-  .d8({1, prog, 0}), 
+  .d8({1'b1, prog, 1'b0}), 
   .an(an), 
   .dec_cat(dec_ddp)
 );
