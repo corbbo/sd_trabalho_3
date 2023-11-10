464bb61a0afc110dbe0ca35a817fc5c3a3e9d34c
corbbo <thiagozilber@gmail.com>
2023-11-10 16:33:02 -0300

    correcao do fib

--- fibonacci.v
+++ fibonacci.v
@@ -5,28 +5,25 @@ module fibonacci
   output wire [15:0] f_out
 );
 
-reg [15:0] f_n1, f_n2, f_out_reg;
+reg [15:0] f_soma, f_out_reg;
 
 always @(posedge clk or posedge rst) begin
   if (rst) begin
     f_out_reg <= 0;
-    f_n1 <= 0;
-    f_n2 <= 0;
+    f_soma <= 0;
   end else if (f_en) begin
-    if (f_out == 0) begin
+    if (f_out_reg == 0) begin
       f_out_reg <= 16'd1;
-    end else if (f_out == 16'd1) begin
-      f_out_reg <= 16'd1;
-      f_n1 <= 16'd1;
+    end else if (f_soma == 0) begin
+      f_soma <= 16'd1;
     end else begin
-      f_out_reg <= f_n1 + f_n2;
-      f_n2 <= f_n1;
-      f_n1 <= f_out;
+      f_out_reg <= f_out_reg + f_soma;
+      f_soma <= f_out_reg;
     end
   end
 end
 
 assign f_valid = f_en;
-assign f_out = f_out_reg;
+assign f_out = f_en ? f_out_reg : 0;
 
 endmodule
